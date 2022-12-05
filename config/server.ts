39a981ed0746e03d8434d9183bad611aa6e84f2a
Yash <vibhandikyash@gmail.com>
2022-12-05 15:05:34 +0530

    updated server ip in url

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '10.0.0.1'),
   port: env.int('PORT', 1337),
+  url: 'http://54.167.97.81:1337/',
   app: {
     keys: env.array('APP_KEYS'),
   },
