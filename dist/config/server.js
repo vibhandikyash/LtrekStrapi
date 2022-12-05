05f06af81e1ca6f9f63a7af606e754067f60bee9
Yash <vibhandikyash@gmail.com>
2022-12-05 17:44:14 +0530

    updated server url config

--- dist/config/server.js
+++ dist/config/server.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = ({ env }) => ({
     host: env('HOST', '10.0.0.1'),
     port: env.int('PORT', 1337),
-    url: 'http://54.167.97.81:1337/',
+    url: 'http://35.171.1.40:1337/',
     app: {
         keys: env.array('APP_KEYS'),
     },
