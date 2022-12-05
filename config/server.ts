1142001d0d73103f740ae7f0bba416d8c2211262
Yash <vibhandikyash@gmail.com>
2022-12-05 14:47:03 +0530

    updated host

--- config/server.ts
+++ config/server.ts
@@ -1,5 +1,5 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', '10.0.0.1'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
--- dist/config/server.js
+++ dist/config/server.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = ({ env }) => ({
-    host: env('HOST', '0.0.0.0'),
+    host: env('HOST', '10.0.0.1'),
     port: env.int('PORT', 1337),
     app: {
         keys: env.array('APP_KEYS'),
